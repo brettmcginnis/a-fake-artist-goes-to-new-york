d40f8f79d4676e105597dfc6fbc86e3a9cf4c141
Brett McGinnis <brettmcgin@gmail.com>
2022-02-28 21:54:59 -0800

    clean up

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import rough from 'roughjs'
+import { RoughCanvas } from 'roughjs/bin/canvas'
 import './Canvas.css'
 
 interface Point {
@@ -11,14 +12,54 @@ interface Line {
     points: Array<Point>
 }
 
+const drawLine = (rc: RoughCanvas, points: Point[]) => {
+    points.forEach((v, i) => {
+        const next = points[i + 1]
+
+        if (v && next) {
+            rc.line(v.x, v.y, next.x, next.y)
+        }
+    })
+}
+
 function Canvas() {
     const [height, setHeight] = useState(640)
     const [width, setWidth] = useState(640)
-    const divRef = useRef(null)
 
-    const canvasRef = useRef(null)
+    const parentDivRef = useRef<HTMLDivElement>(null)
+    const canvasRef = useRef<HTMLCanvasElement>(null)
 
     const [isMouseDown, setIsMouseDown] = useState(false)
+
+    const [lines, setLines] = useState(Array<Line>())
+    const [currentLine, setCurrentLine] = useState<Line>({ points: Array<Point>() })
+
+    const getRough = (): RoughCanvas => rough.canvas(canvasRef.current)
+
+    const getCanvasOffset = (): {left :number, top :number} => {
+        const rect = canvasRef?.current?.getBoundingClientRect()
+        
+        return {
+            left: rect?.left || 0,
+            top: rect?.top || 0
+        }
+    }
+
+    const addPoint = (x: number, y: number) => {
+        if (isMouseDown) {
+            setCurrentLine({ points: [...currentLine.points, { x, y }] })
+        }
+    }
+
+    const setCanvasDimensions = () => {
+        const currentHeight: number = parentDivRef?.current?.clientHeight || 640
+        const calculatedWidth = (currentHeight / 16) * 9
+        setHeight(currentHeight)
+        setWidth(calculatedWidth)
+    }
+
+    const setMouseUp = () => setIsMouseDown(false)
+
     const setMouseDown = () => {
         setIsMouseDown(true)
 
@@ -27,53 +68,15 @@ function Canvas() {
             setCurrentLine({ points: Array<Point>() })
         }
     }
-    const setMouseUp = () => setIsMouseDown(false)
 
-    const [lines, setLines] = useState(Array<Line>())
-    const [currentLine, setCurrentLine] = useState<Line>({ points: Array<Point>() })
-
-    console.log({ lines })
-
-    useEffect(() => {
-        const currentHeight: number = divRef?.current?.clientHeight || 640
-        const calculatedWidth = (currentHeight / 16) * 9
-        console.log({height: currentHeight, width: calculatedWidth})
-        setHeight(currentHeight)
-        setWidth(calculatedWidth)
-    }, [])
-
-    useEffect(() => {
-        console.log('canvas')
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
-
-        const rc = rough.canvas(canvas);
-
-        const drawLine = (points: Point[]) => {
-            points.forEach((v, i) => {
-                const next = points[i + 1]
-
-                if (v && next) {
-                    rc.line(v.x, v.y, next.x, next.y)
-                }
-            })
-        }
-
-        lines.forEach((line) => drawLine(line.points))
-        drawLine(currentLine.points)
-
-    }, [lines, currentLine]) // seperate
-
+    useEffect(() => setCanvasDimensions(), [])
     useEffect(() => document.addEventListener("touchmove", (e) => { e.preventDefault() }, { passive: false }), [])
 
-    const addPoint = (x, y) => {
-        if (isMouseDown) {
-            setCurrentLine({ points: [...currentLine.points, { x, y }] })
-        }
-    }
+    useEffect(() => lines.forEach((line) => drawLine(getRough(), line.points)), [lines])
+    useEffect(() => drawLine(getRough(), currentLine.points), [currentLine])
 
     return (
-        <div className='Painting' ref={divRef}>
+        <div className='Painting' ref={parentDivRef}>
             <canvas
                 width={width}
                 height={height}
@@ -82,24 +85,17 @@ function Canvas() {
                 onMouseUp={() => setMouseUp()}
                 onMouseOut={() => setMouseUp()}
                 onMouseMove={(e) => {
-                    const rect = canvasRef.current.getBoundingClientRect()
-
-                    addPoint(e.clientX - rect.left, e.clientY - rect.top)
-                    console.log({ x: e.clientX - rect.left, y: e.clientY - rect.top })
+                    const {left, top} = getCanvasOffset()
+                    addPoint(e.clientX - left, e.clientY - top)
                 }}
                 onTouchStart={() => setMouseDown()}
                 onTouchEnd={() => setMouseUp()}
                 onTouchMove={(e) => {
                     console.log({ touchCount: e.touches.length, e })
                     if (e.touches.length > 0) {
-                        const rect = canvasRef.current.getBoundingClientRect()
-                        const x = e.touches[0].clientX
-                        const y = e.touches[0].clientY
-                        // console.log({x,y})
-                        addPoint(x - rect.left, y - rect.top)
+                        const {left, top} = getCanvasOffset()
+                        addPoint(e.touches[0].clientX - left, e.touches[0].clientY - top)
                     }
-                    // const rect = canvasRef.current.getBoundingClientRect();
-
                 }}
             />
         </div>
